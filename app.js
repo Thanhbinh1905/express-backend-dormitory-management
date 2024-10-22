7221d82493d1095bb84740e31134437527d8677c
Trần Thanh Bình <thanhbinh2003195@gmail.com>
2024-10-23 06:31:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // Middleware để parse JSON
 app.use(express.json());
